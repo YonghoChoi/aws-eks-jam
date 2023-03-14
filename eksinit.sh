c4a6ac282cbcab506b7e7bfc0215c5107d2e18d9
YonghoChoi <yongho1037@gmail.com>
2023-03-14 16:05:18 +0000

    update eks init script

--- eksinit.sh
+++ eksinit.sh
@@ -14,12 +14,13 @@
 # SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #
 
-#title           eksinit-jam.sh
-#description     This script will setup the Cloud9 IDE with the prerequisite packages and code for the EKS JAM for AppMod
-#author          YoungJoon Jeong (@yjeong)
-#date            2023-02-22
-#version         0.1
-#Refrence        origin from eksinit.sh from event engine boot strapping 
+#This script is revised to use with eks-security workshop and below information is for origin.
+#title           eksinit.sh
+#description     This script will setup the Cloud9 IDE with the prerequisite packages and code for the EKS workshop.
+#author          Imaya Kumar Jagannathan (@ijaganna)
+#contributors    Rob Solomon (@rosolom)
+#date            2022-04-18
+#version         1.2
 #usage           aws s3 cp s3://ee-assets-prod-us-east-1/modules/bd7b369f613f452dacbcea2a5d058d5b/v5/eksinit.sh . && chmod +x eksinit.sh && ./eksinit.sh ; source ~/.bash_profile ; source ~/.bashrc
 #==============================================================================
 
@@ -34,9 +35,7 @@ sudo yum history new
 sudo yum -y -q install jq
 
 # Install yq (yaml query)
-echo 'yq() {
-  docker run --rm -i -v "${PWD}":/workdir mikefarah/yq "$@"
-}' | tee -a ~/.bashrc && source ~/.bashrc
+wget https://github.com/mikefarah/yq/releases/download/v4.2.0/yq_linux_amd64.tar.gz -O - | tar xz && sudo mv yq_linux_amd64 /usr/bin/yq
 
 # Install other utils:
 #   gettext: a framework to help other GNU packages product multi-language support. Part of GNU Translation Project.
@@ -48,13 +47,16 @@ sudo yum -y install gettext bash-completion moreutils
 pip install --user --upgrade awscli
 
 # Install awscli v2
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
+curl -O "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip"
+unzip -o awscli-exe-linux-x86_64.zip
 sudo ./aws/install
-rm awscliv2.zip
+rm awscli-exe-linux-x86_64.zip
 
 # Configure Cloud9 creds
 aws cloud9 update-environment  --environment-id $C9_PID --managed-credentials-action DISABLE
+
+# Enable GuardDuty
+aws guardduty create-detector --enable
 
 # Install kubectl
 #  enable desired version by uncommenting the desired version below:
@@ -74,32 +76,9 @@ curl -o /tmp/kubectl https://s3.us-west-2.amazonaws.com/amazon-eks/1.24.10/2023-
 sudo mv /tmp/kubectl /usr/local/bin
 chmod +x /usr/local/bin/kubectl
 
-
-# set kubectl as executable, move to path, populate kubectl bash-completion
-chmod +x kubectl && sudo mv kubectl /usr/local/bin/
-echo "source <(kubectl completion bash)" >> ~/.bashrc
-echo "source <(kubectl completion bash | sed 's/kubectl/k/g')" >> ~/.bashrc
-
-# Install c9 for editing files in cloud9
-npm install -g c9
-
 # Install eksctl and move to path
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 sudo mv /tmp/eksctl /usr/local/bin
-
-# Install helm
-curl -sSL https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
-
-# Install k9s
-curl -Lo k9s.tgz https://github.com/derailed/k9s/releases/download/v0.27.3/k9s_Linux_amd64.tar.gz
-tar -xf k9s.tgz
-sudo install k9s /usr/local/bin/
-
-# Install kns and ktx
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install --all
-sudo curl https://raw.githubusercontent.com/blendle/kns/master/bin/kns -o /usr/local/bin/kns && sudo chmod +x $_
-sudo curl https://raw.githubusercontent.com/blendle/kns/master/bin/ktx -o /usr/local/bin/ktx && sudo chmod +x $_
 
 # Install aliases
 echo "alias k='kubectl'" | tee -a ~/.bashrc
@@ -107,22 +86,9 @@ echo "alias kgp='kubectl get pods'" | tee -a ~/.bashrc
 echo "alias kgsvc='kubectl get svc'" | tee -a ~/.bashrc
 echo "alias kgn='kubectl get nodes -L beta.kubernetes.io/arch -L eks.amazonaws.com/capacityType -L beta.kubernetes.io/instance-type -L eks.amazonaws.com/nodegroup -L topology.kubernetes.io/zone -L karpenter.sh/provisioner-name -L karpenter.sh/capacity-type'" | tee -a ~/.bashrc
 
-# Clone lab repositories
-#cd ~/environment
-#git clone https://github.com/brentley/ecsdemo-frontend.git
-#git clone https://github.com/brentley/ecsdemo-nodejs.git
-#git clone https://github.com/brentley/ecsdemo-crystal.git
-#git clone https://github.com/aws-containers/eks-app-mesh-polyglot-demo.git
-
-
-######################
+#####################
 ##  Set Variables  ##
 #####################
-
-# Set AWS LB Controller version
-echo 'export LBC_VERSION="v2.4.1"' >>  ~/.bash_profile
-echo 'export LBC_CHART_VERSION="1.4.1"' >>  ~/.bash_profile
-.  ~/.bash_profile
 
 # Set AWS region in env and awscli config
 AWS_REGION=$(curl --silent http://169.254.169.254/latest/dynamic/instance-identity/document | jq -r .region)
@@ -137,6 +103,11 @@ echo "export ACCOUNT_ID=${ACCOUNT_ID}" | tee -a ~/.bash_profile
 EKS_CLUSTER_NAME=$(aws eks list-clusters --region ${AWS_REGION} --query clusters --output text)
 echo "export EKS_CLUSTER_NAME=${EKS_CLUSTER_NAME}" | tee -a ~/.bash_profile
 echo "export CLUSTER_NAME=${EKS_CLUSTER_NAME}" | tee -a ~/.bash_profile
+
+# Install Kubeseal
+wget https://github.com/bitnami-labs/sealed-secrets/releases/download/v0.19.5/kubeseal-0.19.5-linux-amd64.tar.gz
+tar -xvzf kubeseal-0.19.5-linux-amd64.tar.gz kubeseal
+sudo install -m 755 kubeseal /usr/local/bin/kubeseal
 
 # Update kubeconfig and set cluster-related variables if an EKS cluster exists
 
@@ -171,15 +142,23 @@ then
 
 fi
 
-# Update aws-auth ConfigMap granting cluster-admin to TeamRole (cluster creator is "eksworkshop-admin")
-eksctl create iamidentitymapping \
-  --cluster ${EKS_CLUSTER_NAME} \
-  --arn arn:aws:iam::${ACCOUNT_ID}:role/TeamRole \
-#  --arn arn:aws:sts::${AWS::AccountId}:assumed-role/${LabUserRoleName}/team-console \
-  --username cluster-admin \
-  --group system:masters \
-  --region ${AWS_REGION}
+export AWS_REGION=$(curl -s 169.254.169.254/latest/dynamic/instance-identity/document | jq -r '.region')
+echo "export AWS_REGION=${AWS_REGION}" | tee -a ~/.bash_profile
+aws configure set default.region ${AWS_REGION}
+export ACCOUNT_ID=$(curl -s 169.254.169.254/latest/dynamic/instance-identity/document | jq -r '.accountId')
+echo "export ACCOUNT_ID=${ACCOUNT_ID}" | tee -a ~/.bash_profile
+export EKS_NODE_ROLE_NAME=$(aws iam list-instance-profiles | jq -r '.InstanceProfiles | .[] .Roles | .[] .RoleName' | grep eksctl-${EKS_CLUSTER_NAME}-nodegroup-nod-NodeInstanceRole)
+echo "export EKS_NODE_ROLE_NAME=${EKS_NODE_ROLE_NAME}" | tee -a ~/.bash_profile
+mkdir /home/ec2-user/environment/tmp
+mkdir /home/ec2-user/environment/challenge
 
+# Download Task Deployment Files
+curl 'https://raw.githubusercontent.com/YonghoChoi/aws-eks-jam/main/k8s/sockshop/deployment.yml' --output /home/ec2-user/environment/challenge/deployment.yaml
+
+# Create and update eks token continously
+curl 'https://s3.ap-northeast-2.amazonaws.com/do-not-delete-demo-website.ap-northeast-2/eks-challenge/token.sh' --output /tmp/token.sh
+chmod 700 /tmp/token.sh
+sh /tmp/token.sh &
 
 # cleanup
 rm -vf ${HOME}/.aws/credentials
